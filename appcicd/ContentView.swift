befd1bf574a61629bb613252641f053b383be471
AugustoRoa <augusto.roa@pragma.com.co>
2023-02-17 00:09:22 -0500

    Initial Commit

---
+++ appcicd/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  appcicd
+//
+//  Created by Dieber A Roa M on 16/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appcicd/appcicdApp.swift
@@ -0,0 +1,17 @@
+//
+//  appcicdApp.swift
+//  appcicd
+//
+//  Created by Dieber A Roa M on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct appcicdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
